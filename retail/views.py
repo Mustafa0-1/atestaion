bd34248899dab7ee0a325edcc07baaad2a05236f
mustafanysan <mustafa.nysan@mail.ru>
2023-04-21 18:02:11 +0600

    views

--- retail/views.py
+++ retail/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django_filters.rest_framework import DjangoFilterBackend
+# from django.shortcuts import render
+# from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from retail.filter import RetailCountryFilter
